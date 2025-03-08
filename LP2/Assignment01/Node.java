48cb340a1f24e6f65ad2f50f4c94cb3a719db122
Farkhanda-Dalal <125860512+Farkhanda-Dalal@users.noreply.github.com>
2025-03-08 16:16:27 +0530

    Create Node.java

---
+++ LP2/Assignment01/Node.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+// Node of the Graph
+class Node {
+    int dest;
+    Node next;
+
+    public Node(int data) {
+        this.dest = data;
+        this.next = null;
+    }
+}
+
